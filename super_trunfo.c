21e614f37d819ffde2c81bd7c3ebbaa78f669335
cconectai <cconectai100@outlook.com>
2025-04-23 11:15:30 -0300

    Create super_trunfo.c

---
+++ super_trunfo.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main(){
+
+char Cidade;
+int Populacao;
+float Area;
+float PIB;
+int Pontos turistico;
+
+printf("Digite sua cidade: \n");
+scanf("%d", &cidade);
+
+printf("Digite sua populacao: n\");
+scanf("%d", &populacao);
+
+printf("Digite sua area: n\");
+scanf("%f", &area);
+
+printf("Digite seu PIB: \n");
+scanf("%f", &PIB);
+
+printf("Digite pontos turisticos: \n");
+scanf("%d", &pontos turisticos);
+
+return0;
+
+
+}
+
+
+
